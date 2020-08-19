1ab7e219f886c0c4f860ad961aee834c8ff4cabc
Tomoyuki Hirose <still0529@gmail.com>
2020-08-20 02:09:41 +0900

    受講者トップ画面のマークアップ

---
+++ resources/views/top.blade.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>受講者トップ</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+    </head>
+    <body>
+        <header class="header">
+            <div class="logo">
+                <img><!-- Todo:ロゴ画像入れる --></img>
+            </div>
+            <h1>
+                Narrative Career School Online
+            </h1>
+
+            <nav>
+                <ul>
+                    <li>
+                        <a href="">通知<!-- Todo: アイコン入れる --></a>
+                    </li>
+                    <li>
+                        <a href="">プロフィール<!-- Todo: アイコン入れる --></a>
+                    </li>
+                    <li>
+                        <a href="">フォーム<!-- Todo: アイコン入れる --></a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+
+        <main class="current-work">
+            <!-- 背景にバナー入れる -->
+            <div class="content">
+                <p class="heading">受講中のWork</p>
+                <div class="content">
+                    <p>Type A</p>
+                    <img><!-- Todo:画像入れる --></ing>
+                    <p>やりたいことを言語化・具体化し、行動に繋げる</p>
+                    <div><!-- Todo: 進捗バー製作 --></div>
+                </div>
+            </div>
+        </main>
+
+        <main class="all-work">
+            <div class="contents">
+                <p class="heading">Work一覧</p>
+                <div class="works">
+                    <div class="work">
+                        <p class="work-name">Type α</p>
+                        <img><!-- Todo:画像入れる --></img>
+                        <p class="work-desc1">やりたいことを言語化・具体化し、行動に繋げる</p>
+                        <p class="work-desc2">自分のやりたいことが具体的に言語化されれば、その他の行動は大きく変わってきます。....</p>
+                    </div>
+
+                    <div class="work">
+                        <p class="work-name">Type α</p>
+                        <img><!-- Todo:画像入れる --></img>
+                        <p class="work-desc1">やりたいことを言語化・具体化し、行動に繋げる</p>
+                        <p class="work-desc2">自分のやりたいことが具体的に言語化されれば、その他の行動は大きく変わってきます。....</p>
+                    </div>
+
+                    <div class="work">
+                        <p class="work-name">Type α</p>
+                        <img><!-- Todo:画像入れる --></img>
+                        <p class="work-desc1">やりたいことを言語化・具体化し、行動に繋げる</p>
+                        <p class="work-desc2">自分のやりたいことが具体的に言語化されれば、その他の行動は大きく変わってきます。....</p>
+                    </div>
+
+                    <div class="work">
+                        <p class="work-name">Type α</p>
+                        <img><!-- Todo:画像入れる --></img>
+                        <p class="work-desc1">やりたいことを言語化・具体化し、行動に繋げる</p>
+                        <p class="work-desc2">自分のやりたいことが具体的に言語化されれば、その他の行動は大きく変わってきます。....</p>
+                    </div>
+
+                    <div class="work">
+                        <p class="work-name">Type α</p>
+                        <img><!-- Todo:画像入れる --></img>
+                        <p class="work-desc1">やりたいことを言語化・具体化し、行動に繋げる</p>
+                        <p class="work-desc2">自分のやりたいことが具体的に言語化されれば、その他の行動は大きく変わってきます。....</p>
+                    </div>
+
+                    <div class="work">
+                        <p class="work-name">Type α</p>
+                        <img><!-- Todo:画像入れる --></img>
+                        <p class="work-desc1">やりたいことを言語化・具体化し、行動に繋げる</p>
+                        <p class="work-desc2">自分のやりたいことが具体的に言語化されれば、その他の行動は大きく変わってきます。....</p>
+                    </div>
+                </div>
+            </div>
+            <div>
+        </main>
+
+        <main class="recoomend">
+            <div>
+                <p class="heading">あなたにおすすめの情報</p>
+                <div class=contents>
+                    <div class="content">
+                        <p class="content-name">コミュニティ</p>
+                        <img><!-- Todo:画像入れる --></img>
+                    </div>
+
+                    <div class="content">
+                        <p class="content-name">人</p>
+                        <img><!-- Todo:画像入れる --></img>
+                    </div>
+
+                    <div class="content">
+                        <p class="content-name">会社</p>
+                        <img><!-- Todo:画像入れる --></img>
+                    </div>
+
+                    <div class="content">
+                        <p class="content-name">アカデミック</p>
+                        <img><!-- Todo:画像入れる --></img>
+                    </div>
+                </div>
+            </div>
+        </main>
+
+        <main class="about-ncs">
+            <div>
+                <p class="heading">NCSについて</p>
+                <div class=contents>
+                    <div class="content">
+                        <p class="content-name">発起人について</p>
+                        <img><!-- Todo:画像入れる --></img>
+                    </div>
+
+                    <div class="content">
+                        <p class="content-name">NCSのストーリー</p>
+                        <img><!-- Todo:画像入れる --></img>
+                    </div>
+
+                    <div class="content">
+                        <p class="content-name">NCSに関する記事</p>
+                        <img><!-- Todo:画像入れる --></img>
+                    </div>
+                </div>
+            </div>
+        </main>
+
+
+        <footer>
+            <div class="logo">
+                <img><!-- Todo:ロゴ画像入れる --></img>
+            </div>
+
+            <ul>
+                <li><a href="">お問い合わせ</li>
+                <li><a href="">このサイトについて</li>
+                <li><a href="">利用規約</li>
+            </ul>
+            <p>
+                <small>Copyright 2020 Narrative career school</small>
+            </p>
+        </footer>
+    </body>
+</html>
