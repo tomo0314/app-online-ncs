9888a333f084728aacb186ca077b6210e0aaa57f
Tomoyuki Hirose <still0529@gmail.com>
2020-08-25 05:19:24 +0900

    FontAwesomeを適用

--- public/js/app.js
+++ public/js/app.js
@@ -37313,17 +37313,6 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /***/ }),
 
-/***/ "./resources/sass/app.scss":
-/*!*********************************!*\
-  !*** ./resources/sass/app.scss ***!
-  \*********************************/
-/*! no static exports found */
-/***/ (function(module, exports) {
-
-// removed by extract-text-webpack-plugin
-
-/***/ }),
-
 /***/ "./resources/sass/work_top.scss":
 /*!**************************************!*\
   !*** ./resources/sass/work_top.scss ***!
@@ -37336,14 +37325,13 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /***/ }),
 
 /***/ 0:
-/*!********************************************************************************************!*\
-  !*** multi ./resources/js/app.js ./resources/sass/app.scss ./resources/sass/work_top.scss ***!
-  \********************************************************************************************/
+/*!******************************************************************!*\
+  !*** multi ./resources/js/app.js ./resources/sass/work_top.scss ***!
+  \******************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(/*! /var/www/resources/js/app.js */"./resources/js/app.js");
-__webpack_require__(/*! /var/www/resources/sass/app.scss */"./resources/sass/app.scss");
 module.exports = __webpack_require__(/*! /var/www/resources/sass/work_top.scss */"./resources/sass/work_top.scss");
 
 
