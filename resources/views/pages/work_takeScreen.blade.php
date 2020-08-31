8820821f0ce25f29f108c75f383b68118d63af45
RYOISHI <ryo.ishigami.1129@gmail.com>
2020-09-01 04:03:24 +0900

    動画受講ページのレイアウト整理

--- resources/views/pages/work_takeScreen.blade.php
+++ resources/views/pages/work_takeScreen.blade.php
@@ -1,11 +1,60 @@
 @extends('layout')
 
 @section('style_content')
-    <!-- Todo : このページ用のCSSファイルにパスを変更する -->
     <link href="{{ mix('css/work_top.css') }}" rel="stylesheet"> 
 @endsection
 
 @section('content')
-    <!-- BodyのHTMLをここに書く（headerとfooterは自動で継承される） -->
+
+<body>
+    <main class="flex">    
+        <!-- サイドバー -->
+        <nav class="sidebar">
+            <div>
+                <div>サイドバーを閉じるボタン</div>
+                <div>現在受講中のワーク名</div>
+            </div>
+            <div>
+                <ul>
+                    <li>やりたいことを科学する</li>
+                    <li>職業を探す</li>
+                    <li>やりたいことを科学する</li>
+                    <li>やりたいことを科学する</li>
+                    <li>やりたいことを科学する</li>
+                    <li>やりたいことを科学する</li>
+                    <li>やりたいことを科学する</li>
+                </ul>
+            </div>
+        </nav>
+
+        <!-- 動画エリア -->
+        <section class="">
+            <div>動画</div>
+            <div>
+                <p>ナビゲーション</p>
+                <ul>
+                    <div>
+                        <li>概要</li>
+                        <li>Q&amp;A</li>
+                    </div>
+                    <div>
+                        <li>メンターに相談してみる</li>
+                        <li>資料ダウンロード</li>
+                    </div>
+                </ul>
+            </div>
+        </section>
+
+        <!-- メモエリア -->
+        <section class="">
+            <div>メモ</div>
+            <div>
+                <p>Q2. キャリアガーデンのサイトを開き、自分の好きな職業を抜き出そう！</p>
+                <textarea rows="10" cols="60" placeholder="ここに入力してみよう！"></textarea>
+            </div>
+        </section>
+    </main>
+</body>
+
 @endsection
 
