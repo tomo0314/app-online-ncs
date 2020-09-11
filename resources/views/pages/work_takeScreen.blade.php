1cee96cec0fd7ab6a21b77a956ba0a6f6952bb4f
RYOISHI <ryo.ishigami.1129@gmail.com>
2020-09-12 00:46:12 +0900

    レイアウト修正
    一応レスポンシブになっている、はず・・・

--- resources/views/pages/work_takeScreen.blade.php
+++ resources/views/pages/work_takeScreen.blade.php
@@ -10,59 +10,83 @@
     <main class="container">    
         <!-- サイドバー -->
         <nav class="sidebar">
-            <div>
-                <div class="sidebar__closeButton">×</div>
-                <div class="sidebar__workTitle">TypeA:やりたいことを科学する</div>
+            <div class="titleArea">
+                <button class="sidebar__closeButton"> × </button>
+                <div class="sidebar__workTitle">TypeA:やりたいことを科学する
+                </div>
             </div>
-            <div>
-                <ul>
-                    <li>職業を探す</li>
-                    <li>やりたいことを科学する</li>
-                    <li>やりたいことを科学する</li>
-                    <li>やりたいことを科学する</li>
-                    <li>やりたいことを科学する</li>
-                    <li>やりたいことを科学する</li>
-                </ul>
+            <div class="sidebar__progress">
+                <table>
+                    <tr>
+                        <td><input type="checkbox"></td>
+                        <td>やりたいことを科学する</td>
+                    </tr>
+                    <tr>
+                        <td><input type="checkbox"></td>
+                        <td>やりたいことを科学する</td>
+                    </tr>
+                    <tr>
+                        <td><input type="checkbox"></td>
+                        <td>やりたいことを科学する</td>
+                    </tr>
+                    <tr>
+                        <td><input type="checkbox"></td>
+                        <td>やりたいことを科学する</td>
+                    </tr>
+                    <tr>
+                        <td><input type="checkbox"></td>
+                        <td>やりたいことを科学する</td>
+                    </tr>                    <tr>
+                        <td><input type="checkbox"></td>
+                        <td>やりたいことを科学する</td>
+                    </tr>
+                </table>
             </div>
         </nav>
 
-        <!-- 動画エリア -->
-        <section class="movieArea">
-            <div class="movieArea__nav-header">
-                <div class="title">職業を探す</div>
-                <div class="button">
-                    <button class="button__back">＜</button>
-                    <button class="button__forward">＞</button>
-                </div>
-            </div>
-            
-            <div class="movieArea__movie">
-                <iframe width="100%" height="315" src="https://www.youtube.com/embed/Nnqgd2k4-ic" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            </div>
-                
-            <div class="movieArea__nav-footer">
-                <ul>
-                    <div >
-                        <li>概要</li>
-                        <li>Q&amp;A</li>
-                    </div>
-                    <div>
-                        <li>メンターに相談してみる</li>
-                        <li>資料ダウンロード</li>
-                    </div>
-                </ul>
-            </div>
-        </section>
+        <!-- 動画＆メモエリア -->
+        <section class="contentsArea">
 
-        <!-- メモエリア -->
-        <section class="memoArea">
-            <div class="titleArea">メモ</div>
-            <div class="contentsArea">
-                <p>Q2. キャリアガーデンのサイトを開き、自分の好きな職業を抜き出そう！</p>
-                <textarea class="memo__textarea" rows="10" placeholder="ここに入力してみよう！"></textarea>
-                <button class="saveButton">保存する</button>
-            </div>
-        </section>
+            <!-- タイトルバー -->
+            <section class="titleArea">
+                <div class="title__workName">職業を探す</div>
+                <div class="button__backForward">
+                    <button class="button button__back">＜</button>
+                    <button class="button button__forward">＞</button>
+                </div>
+                <div class="title__memo">メモ</div>
+            </section>
+                
+            
+            <!-- 動画エリア -->
+            <section class="contents__movieAndMemo">
+
+                <section class="movieArea">
+                    <div class="movieArea__movie">
+                        <iframe width="100%" height="315" src="https://www.youtube.com/embed/Nnqgd2k4-ic" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    </div>
+                    <div class="movieArea__nav">
+                        <div class="left">
+                            <div>概要</div>
+                            <div>Q&A</div>
+                        </div>
+                        <div class="right">
+                            <div>メンターに相談してみる</div>
+                            <div>資料ダウンロード</div>
+                        </div>
+                    </div>
+                </section>
+                <section class="memoArea">
+                    <div class="memoTitle">Q2. キャリアガーデンのサイトを開き、自分の好きな職業を抜き出そう！</div>
+                    <div class="text-and-saveButton">
+                        <textarea class="textarea" rows="10" placeholder="ここに入力してみよう！"></textarea>
+                        <button class="saveButton">保存する</button>
+                    </div>
+                </section>
+
+            
+            </section>
+                    
     </main>
 </body>
 
